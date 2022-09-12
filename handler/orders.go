0efbc8d2ca687d5c9c57c104e6b05c5686424a4e
Gidhan <gidhanbagusalgary@gmail.com>
2022-09-12 16:53:50 +0700

    Add swagger docs

--- handler/orders.go
+++ handler/orders.go
@@ -16,6 +16,16 @@ func NewHandler(service orders.Service) *orderHandler {
 	return &orderHandler{service}
 }
 
+// @BasePath /api/v1
+
+// FindAll godoc
+// @Summary Show all orders
+// @Description Get all orders
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Success 200 {json} helper.APIResponse
+// @Router /orders [get]
 func (h *orderHandler) FindAll(c *gin.Context) {
 	findOrders, err := h.service.FindAll()
 	if err != nil {
@@ -28,6 +38,15 @@ func (h *orderHandler) FindAll(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
+// FindAllByUserID godoc
+// @Summary Show all orders by user id
+// @Description Get all orders by user id
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param user_id path int true "User ID"
+// @Success 200 {json} helper.APIResponse
+// @Router /orders/user/{user_id} [get]
 func (h *orderHandler) FindByID(c *gin.Context) {
 	var input orders.FindOrderInput
 	err := c.ShouldBindUri(&input)
@@ -50,6 +69,15 @@ func (h *orderHandler) FindByID(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
+// Save godoc
+// @Summary Create new order
+// @Description Create new order
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order body orders.SaveOrderInput true "Order"
+// @Success 200 {json} helper.APIResponse
+// @Router /orders [post]
 func (h *orderHandler) Save(c *gin.Context) {
 	var input orders.SaveOrderInput
 
@@ -73,6 +101,16 @@ func (h *orderHandler) Save(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
+// Update godoc
+// @Summary Update order
+// @Description Update order
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order_id path int true "Order ID"
+// @Param order body orders.UpdateOrderInput true "Order"
+// @Success 200 {json} helper.APIResponse
+// @Router /orders/{order_id} [put]
 func (h *orderHandler) Update(c *gin.Context) {
 	var inputID orders.FindOrderInput
 	err := c.ShouldBindUri(&inputID)
@@ -105,6 +143,15 @@ func (h *orderHandler) Update(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
+// Delete godoc
+// @Summary Delete order
+// @Description Delete order
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order_id path int true "Order ID"
+// @Success 200 {json} helper.APIResponse
+// @Router /orders/{order_id} [delete]
 func (h *orderHandler) Delete(c *gin.Context) {
 	var input orders.FindOrderInput
 	err := c.ShouldBindUri(&input)
